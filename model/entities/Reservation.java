d22e340a1e6c5bf1dafac3eac48a8835cffed60d
Israel Teixeira de Andrade <israeltandrade@hotmail.com>
2023-11-15 08:44:11 -0300

    very bad solution

---
+++ model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package excecoes.exceptions1.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	// Dado estático para não ser um objeto novo para cada objeto instanciado:
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public Date getCheckIn() {
+		return checkIn;
+	}
+	
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	// O cálculo com datas através do método "getTime()retorna um valor do tipo long, por isso o valor
+	// de retorno deste método também ser do tipo long:
+	public long duration() {
+		// A diferença será trazida em milisegundos:
+		long diff = checkOut.getTime() - checkIn.getTime();
+		// TimeUnit é um Enum com operações, aqui, convertendo "diff" de milisegundos para dias:
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ getRoomNumber()
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+	
+}
